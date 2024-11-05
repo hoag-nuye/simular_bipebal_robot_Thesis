d013e48772d522fc5898dd1e56622761ca4e3d1b
hnguyenboy2001 <nguyen80o08nguyen@gmail.com>
2024-11-06 02:02:54 +0700

    build env class next...

--- env/mujoco_env.py
+++ env/mujoco_env.py
@@ -3,6 +3,12 @@ import time
 import mujoco
 import mujoco.viewer
 
+import os
+
+from xml.etree import ElementTree as ET
+from env.mujoco_agt import Agent
+
+
 class Environment:
     # --------------Initial environment--------------------
     def __init__(self, env_xml, show_left_ui=True, show_right_ui=True):
@@ -55,5 +61,59 @@ class Environment:
 
             if time_until_next_step > 0:
                 time.sleep(time_until_next_step)
+
+    # --------------------Add a new agent----------------------
+    def add_agent(self, agent, agent_name):
+        # check the agent type
+        if isinstance(agent, Agent):
+            raise TypeError(f'Warning: the "agent" parameter must be an Agent object!')
+
+        # check the agent exists or not
+        if agent in self.agents:
+            raise ValueError(f'Warning: The agent named "{agent_name}" existed!')
+
+        # check the XML path of agent
+        if os.path.exists(agent.agent_xml):
+            raise FileNotFoundError(f"Warning: Having not found the XML file for the agent with the path is '{agent.agent_xml}!'.")
+
+        # Thêm thông tin include vào file XML của môi trường
+        self.__include_agent_in_xml(agent.agent_xml, agent_name)
+
+        # Lưu agent vào danh sách agents
+        self.agents[agent_name] = agent
+        print(f"Success: Agent '{agent_name}' đã được thêm vào môi trường!")
+
+        # Reload lại model để cập nhật với agent mới
+        self.__reload_model()
+
+    # include an agent in XML file
+    def __include_agent_in_xml(self, agent_xml, agent_name):
+        # Phân tích env xml
+        tree = ET.parse(self.env_xml)
+        # tìm root của agent
+        root = tree.getroot(self.env_xml)
+        # create element
+        include_element = ET.Element("include", file=agent_xml)
+        include_element.attrib['name'] = agent_name # tạo tên cho agent
+        # find 'worldbody' element to add agent
+        worldbody = root.find("worldbody")
+        if worldbody is None:
+            raise ValueError("Không tìm thấy worldbody trong XML của môi trường.")
+        worldbody.append(include_element)
+        # write again xml file of env
+        tree.write(self.env_xml)
+
+    def __reload_model(self):
+        # Cập nhật lại model và data của môi trường sau khi thêm agent
+        self.env_model = mujoco.MjModel.from_xml_path(self.env_xml)
+        self.env_data = mujoco.MjData(self.env_model)
+        self.viewer = mujoco.viewer.MjViewer(self.env_model, self.env_data)
+
+
+
+
+
+
+
 
 
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from env.mujoco_env import Environment
 
-# initial environment
-env_xml_path = 'structures/environment.xml'
-env = Environment(env_xml_path)
 
-with env.viewer:
-    env.render()
+def main():
+    # initial environment
+    env_xml_path = 'structures/environment.xml'
+    env = Environment(env_xml_path)
+
+    # starting simulation
+    with env.viewer:
+        env.render()
+
+if __name__ == "__main__":
+    # main()
+    print()
 
 
