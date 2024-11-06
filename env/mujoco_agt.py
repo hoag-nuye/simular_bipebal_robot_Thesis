eba336e329dede3a0beb7f01fc394d6d7f227510
hnguyenboy2001 <hnguyenboy2001@gmail.com>
2024-11-06 21:38:29 +0700

    Done the agent viewing

--- env/mujoco_agt.py
+++ env/mujoco_agt.py
@@ -1,3 +1,20 @@
 class Agent:
-    def __init__(self):
-        print()
+    def __init__(self, agent_xml, env_data):
+        """
+        Khởi tạo một Agent từ XML và môi trường `env_data`.
+        agent_xml: Đường dẫn tới tệp XML của agent.
+        env_data: Dữ liệu của môi trường MuJoCo.
+        """
+        self.agent_xml = agent_xml
+        self.env_data = env_data
+        # Lưu dữ liệu state hiện tại của agent
+        self.agent_pos = None
+        self.agent_vel = None
+        # Lưu trữ tập trải nghiệm
+        self.trajectory_buffer = []
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from env.mujoco_env import Environment
+from env.mujoco_agt import Agent
 
 
 def main():
-    # initial environment
-    env_xml_path = 'structures/environment.xml'
+    # -------------- initial environment --------------------
+    env_xml_path = 'structures/agility_cassie/environment.xml'
     env = Environment(env_xml_path)
+
+    # -------------- Add a new agent ---------------------
+    agility_cassie_agent = Agent('structures/agility_cassie/cassie.xml', env.env_data)
+    env.add_agent(agility_cassie_agent, 'cassie')
 
     # starting simulation
     with env.viewer:
         env.render()
 
 if __name__ == "__main__":
-    # main()
+    main()
     print()
 
 
